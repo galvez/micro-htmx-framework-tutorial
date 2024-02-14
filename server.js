e548603a827774bece7e8591b984716c161d9840
Jonas Galvez <jonasgalvez@gmail.com>
2024-02-14 05:00:15 -0300

    add Fastify server and basic SSR

---
+++ server.js
@@ -0,0 +1,35 @@
+import Fastify from 'fastify'
+import FastifyVite from '@fastify/vite'
+import FastifyFormBody from '@fastify/formbody'
+
+const server = Fastify({
+  logger: {
+    transport: {
+      target: '@fastify/one-line-logger'
+    }
+  }
+})
+
+await server.register(FastifyFormBody)
+await server.register(FastifyVite,  {
+  root: import.meta.url,
+  createRenderFunction() {},
+  createRoute ({ handler, errorHandler, route }, fastify, config) {
+    fastify.route({
+      url: route.path,
+      method: route.method ?? 'GET',
+      async handler (req, reply) {
+      	console.log('>', await route.default({ fastify, req, reply }))
+        reply.type('text/html')
+        reply.html({
+          element: await route.default({ fastify, req, reply }),
+        })
+      },
+      errorHandler,
+      ...route
+    })
+  }
+})
+
+await server.vite.ready()
+await server.listen({ port: 3000 })
